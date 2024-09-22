27ca48f5b2c560e289ece7c420dc0eb5e45ea9cc
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-09-23 00:33:38 +0500

    Скопировал шаблон проекта

---
+++ src/main/java/ru/urfu/Main.java
@@ -0,0 +1,10 @@
+package ru.urfu;
+
+/**
+ * Основной класс для запуска приложения
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
