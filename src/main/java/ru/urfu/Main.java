426907277a1394e4b4206a600b55d01c273cb2ec
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-09-23 17:37:21 +0500

    Написал примитивного эхо-бота по примерам из документации

--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -1,10 +1,25 @@
 package ru.urfu;
 
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+
 /**
  * Основной класс для запуска приложения
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String botToken = System.getenv("MATHMECHBOT_TOKEN");
+        if (botToken == null) {
+            System.out.println("Couldn't retrieve bot token from MATHMECHBOT_TOKEN");
+            return;
+        }
+
+        try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
+            botsApplication.registerBot(botToken, new EchoBot(botToken));
+            System.out.println("EchoBot successfully started!");
+
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
