e6c4ee52688c53f3d3197b3fbb2cf0150a7726c5
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-09-24 02:04:51 +0500

    Настроил логирование (log4j2.xml взял без изменений из курса по Java)

--- src/main/java/ru/urfu/Main.java
+++ src/main/java/ru/urfu/Main.java
@@ -1,25 +1,29 @@
 package ru.urfu;
 
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Основной класс для запуска приложения
  */
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         String botToken = System.getenv("MATHMECHBOT_TOKEN");
         if (botToken == null) {
-            System.out.println("Couldn't retrieve bot token from MATHMECHBOT_TOKEN");
+            LOGGER.error("Couldn't retrieve bot token from MATHMECHBOT_TOKEN.");
             return;
         }
 
         try (TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication()) {
             botsApplication.registerBot(botToken, new EchoBot(botToken));
-            System.out.println("EchoBot successfully started!");
+            LOGGER.info("EchoBot successfully started!");
 
             Thread.currentThread().join();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("Exception during bot registration.", e);
         }
     }
 }
