05b8d1808c041e60b524ee7115c5d4c91273d400
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-09-23 23:51:35 +0500

    Тесты для EchoBotCore

--- src/main/java/ru/urfu/EchoBotCore.java
+++ src/main/java/ru/urfu/EchoBotCore.java
@@ -1,6 +1,7 @@
 package ru.urfu;
 
 public class EchoBotCore extends LogicCore {
+
     public EchoBotCore() {}
 
     public Message processMessage(Message msg) {
@@ -19,11 +20,12 @@ public class EchoBotCore extends LogicCore {
     }
 
     private Message helpCommandHandler(Message inputMessage) {
-        return new Message("""
-            Привет, я эхо бот! Сейчас я расскажу как ты можешь со мной взаимодействовать.
-             \
-            Я пишу твое сообщение тебе обратно но добавляю фразу 'Ты написал:' в начало твоего сообщения!
-            В любой момент ты можешь написать команду '/help' (без кавычек) и \
-            тогда я тебе напомню как со мной работать! Приятного использования!""");
+        String HELP_MESSAGE = """
+                Привет, я эхо бот! Сейчас я расскажу как ты можешь со мной взаимодействовать.
+                 \
+                Я пишу твое сообщение тебе обратно но добавляю фразу 'Ты написал:' в начало твоего сообщения!
+                В любой момент ты можешь написать команду '/help' (без кавычек) и \
+                тогда я тебе напомню как со мной работать! Приятного использования!""";
+        return new Message(HELP_MESSAGE);
     }
 }
