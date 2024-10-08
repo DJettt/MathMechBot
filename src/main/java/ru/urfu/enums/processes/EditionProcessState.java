1a58ebf306ad21b6e5d46259a122495e93d8e79d
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-10-08 21:08:01 +0500

    Добавил состояний процессу редактирования информации.

--- src/main/java/ru/urfu/enums/processes/EditionProcessState.java
+++ src/main/java/ru/urfu/enums/processes/EditionProcessState.java
@@ -2,5 +2,12 @@ package ru.urfu.enums.processes;
 
 // TODO: нормальное описание и реализацию
 public enum EditionProcessState implements ProcessState {
+    CHOOSE,
+    NAME,
+    YEAR,
+    SPECIALTY1,
+    SPECIALTY2,
+    GROUP,
+    MEN,
     CONFIRMATION;
 }
