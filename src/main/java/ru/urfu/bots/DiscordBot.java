e038826d61cbfd8fed3e674f831d98b0a3758105
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-10-20 23:13:07 +0500

    Убрал "пасхалку" из DiscordBot.

--- src/main/java/ru/urfu/bots/DiscordBot.java
+++ src/main/java/ru/urfu/bots/DiscordBot.java
@@ -5,7 +5,6 @@ import java.util.List;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -55,7 +54,6 @@ public final class DiscordBot extends ListenerAdapter implements Bot {
                         )
                 )
                 .setStatus(OnlineStatus.ONLINE)
-                .setActivity(Activity.playing("IDEA Intellij"))
                 .build();
 
         logger.info("Discord bot successfully started!");
