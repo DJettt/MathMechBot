88a1cd3d66b8555ee0ce3d2f8ef3fbf4481b5d88
pribylov <ddgett08@gmail.com>
2024-10-22 06:10:15 +0500

    Оформил тесты. Теперь в них нет прямого обращения к storage.

--- src/test/java/ru/urfu/logics/mathmechbot/TestConstants.java
+++ src/test/java/ru/urfu/logics/mathmechbot/TestConstants.java
@@ -1,7 +1,5 @@
 package ru.urfu.logics.mathmechbot;
 
-import java.util.List;
-import ru.urfu.localobjects.LocalButton;
 import ru.urfu.localobjects.LocalMessage;
 import ru.urfu.localobjects.LocalMessageBuilder;
 
@@ -10,45 +8,11 @@ import ru.urfu.localobjects.LocalMessageBuilder;
  */
 public final class TestConstants {
     public final static String ACCEPT_COMMAND = "/yes";
-    public final static String DECLINE_COMMAND = "/no";
-    public final static String BACK_COMMAND = "/back";
     public final static String INFO_COMMAND = "/info";
     public final static String REGISTER_COMMAND = "/register";
-    public final static String EDIT_COMMAND = "/edit";
-
-    public final static String EDITING_FULL_NAME_COMMAND = "full_name";
-    public final static String EDITING_YEAR_COMMAND = "year";
-    public final static String EDITING_SPECIALITY_COMMAND = "speciality";
-    public final static String EDITING_GROUP_COMMAND = "number_of_group";
-    public final static String EDITING_MEN_COMMAND = "men";
 
     public final LocalMessage acceptMessage = new LocalMessageBuilder().text(ACCEPT_COMMAND).build();
-    public final LocalMessage declineMessage = new LocalMessageBuilder().text(DECLINE_COMMAND).build();
-    public final LocalButton backButton = new LocalButton("Назад", BACK_COMMAND);
 
     public final LocalMessage infoMessage = new LocalMessageBuilder().text(INFO_COMMAND).build();
     public final LocalMessage registerMessage = new LocalMessageBuilder().text(REGISTER_COMMAND).build();
-
-    public final LocalMessage help = new LocalMessageBuilder()
-            .text("""
-                    /start - начало общения с ботом
-                    /help - выводит команды, которые принимает бот
-                    /register - регистрация
-                    /info - информация о Вас
-                    /edit - изменить информацию
-                    /delete - удалить информацию о Вас""")
-            .build();
-
-    public final LocalMessage editMessage = new LocalMessageBuilder().text(EDIT_COMMAND).build();
-
-    public final LocalMessage editingChooseMessage = new LocalMessageBuilder()
-            .text("Что Вы хотите изменить?")
-            .buttons(List.of(
-                    new LocalButton("ФИО", EDITING_FULL_NAME_COMMAND),
-                    new LocalButton("Курс", EDITING_YEAR_COMMAND),
-                    new LocalButton("Направление", EDITING_SPECIALITY_COMMAND),
-                    new LocalButton("Группа", EDITING_GROUP_COMMAND),
-                    new LocalButton("МЕН", EDITING_MEN_COMMAND),
-                    backButton))
-            .build();
 }
