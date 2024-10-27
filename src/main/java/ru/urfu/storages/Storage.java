480df46240ac1a71cc244171a9eb784c7fa36f12
Andrey Shaat <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2024-10-28 01:44:09 +0500

    Оформление JavaDoc'ов.

--- src/main/java/ru/urfu/storages/Storage.java
+++ src/main/java/ru/urfu/storages/Storage.java
@@ -5,12 +5,14 @@ import java.util.Optional;
 
 /**
  * Интерфейс хранилища, некоторой абстракции над данными.
+ *
  * @param <T> тип хранимых значений
  * @param <I> тип идентификатора у хранимого значения
  */
 public interface Storage<T extends Identifiable<I>, I> {
     /**
      * Ищет элемент с данным идентификатором.
+     *
      * @param id идентификатор.
      * @return первый элемент из хранилища с данным идентификатором.
      */
@@ -26,10 +28,9 @@ public interface Storage<T extends Identifiable<I>, I> {
     /**
      * <p>Добавляет элемент в хранилище.</p>
      *
-     * <p>Должен бросать IllegalArgumentException, если
-     * элемент с данным id уже существует внутри хранилища.</p>
-     *
      * @param member элемент, который добавляем.
+     * @throws IllegalArgumentException элемент
+     * с данным id уже существует внутри хранилища.
      */
     void add(T member) throws IllegalArgumentException;
 
